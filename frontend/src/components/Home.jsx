659ad14432730881e11db7aafcdf490727ef7038
Shivaprasad K <144586350+Shivaprasad-SK@users.noreply.github.com>
2025-06-29 19:16:54 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Home.css";
 import io from "socket.io-client";
-const socket = io("http://localhost:3000");
+const socket = io("https://chat-app-5zqy.vercel.app/");
 
 function Home() {
   // const [count, setCount] = useState(0);
